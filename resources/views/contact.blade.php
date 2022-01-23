292848600b3bfc5e41d2d5fd8af65dfca7161d95
unknown <william.w.rocha@gmail.com>
2022-01-22 22:20:37 -0300

    primeirop commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title', 'Contato')
+    
+@section('content')
+    
+
+
+<h1>Esta é a Página de Contato atualizada!!</h1>
+<a href="/">Voltar para Home</a>
+
+@endsection
